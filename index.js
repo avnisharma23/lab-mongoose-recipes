f9bd1d0e8fbf151895c223e6d44fd8cc0c4e09b8
Avni Sharma <avni.sharma262000@gmail.com>
2022-12-15 22:22:10 +0100

    solved lab

--- index.js
+++ index.js
@@ -17,7 +17,50 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+      let myRecipe = Recipe.create({
+        title:'Lentils Soup',
+        level:'Easy Peasy',
+        ingredients:'lentils,garlic,ginger,water',
+        cuisine: 'indian',
+        dishType: 'soup',
+        image : 'https://images.media-allrecipes.com/images/75131.jpg',
+        duration : 15,
+        creator : 'Avni'
+        
+      })
+      .then(myRecipe => console.log("myreceipe",myRecipe.title))
+
   })
+
+  .then(()=>{
+    let manyRecipe = Recipe.insertMany(data);
+    for(let i = 0; i < data.length; i++){
+      console.log(data[i])
+    }
+  })
+
+  
+
+ .then(() => {
+
+    Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"},{duration: 100},{new: true})
+    .then(updated => console.log('updateduration',updated )) 
+    /* console.log('updated successfully') */
+  })  
+
+  .then(() => {
+
+    Recipe.deleteOne({title :'Carrot Cake'})
+    console.log("Deleted sucessfully")
+
+  }) 
+ .then(() => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+      }); 
+  }) 
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
